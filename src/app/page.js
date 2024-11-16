2b50bc225f368e3649e56b315e3d73f26002b483
Derek Huang <derekhuang7@gmail.com>
2024-11-16 10:58:05 -0800

    cool

--- src/app/page.js
+++ src/app/page.js
@@ -18,6 +18,7 @@ export default function Home() {
             Get started by editing <code>src/app/page.js</code>.
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>cool stuff bro</li>
         </ol>
 
         <div className={styles.ctas}>
