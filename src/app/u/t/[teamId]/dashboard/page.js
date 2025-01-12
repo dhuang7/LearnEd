4f5a8760615d9a03aa96cf6f989e3e054c7c29a4
Derek Huang <derekhuang7@gmail.com>
2025-01-11 17:47:45 -0800

    completed cycle count

--- src/app/u/t/[teamId]/dashboard/page.js
+++ src/app/u/t/[teamId]/dashboard/page.js
@@ -2,10 +2,9 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import MemberList from "./memberList";
 import NormsList from "./normsList";
-import AgendaList from "./agendaList";
 
-import AddAgendaModal from "./addAgendaModal";
 import createClient from "@/utils/supabase/server";
+import CyclesTasks from "./cyclesTasks";
 
 
 
@@ -15,15 +14,28 @@ export default async function Agenda({params}) {
 
     const supabase = await createClient();
 
-    const {data: agendas, error: agendasError} = await supabase
-        .from('agendas')
-        .select()
-        .eq('team_id', teamId);
-
     let { data: normsList, error: normsError } = await supabase
         .from('norms')
         .select('*')
         .eq('team_id', teamId);
+
+    const {data: cycles, error: cyclesError} = await supabase
+        .from('pdsa_cycles')
+        .select(`
+            id.count(),
+            stage,
+            change_ideas(
+                projects(
+                    team_id
+                )
+            )
+        `)
+        .eq('change_ideas.projects.team_id', teamId)
+        .not('change_ideas.projects', 'is', null)
+        .not('change_ideas', 'is', null)
+        .order('stage');
+
+    console.log(cycles);
 
     return (
         <Box sx={{width:'100%', height:'100%', display:'flex', flexDirection:'column'}}>
@@ -36,24 +48,17 @@ export default async function Agenda({params}) {
             </Box>
             {/* norms */}
             <Box sx={{width:'100%',  flexGrow:1, overflow:'hidden'}}>
-                <Box sx={{width:'100%', height:'100%', display:'flex'}}>
-                    {/* side dash */}
-                    <Box sx={{width:'33%', height:'100%', display:'flex', flexDirection:'column', pt:'.5rem', boxSizing:'border-box'}}>
-                        {/* member content */}
-                        <Box sx={{pb:'1rem', boxSizing:'border-box', width:'100%', height:'17.5rem', pl:'1rem', pr:'.5rem'}}>
+                <Box sx={{width:'100%', height:'100%'}}>
+                    {/* top dash */}
+                    <Box sx={{width:'100%', display:'flex', height:'20rem', boxSizing:'border-box', px:'.5rem'}}>
+                        <Box sx={{width:'33%', boxSizing:'border-box', px:'.5rem'}}>
                             <MemberList teamId={teamId} />
                         </Box>
-                        {/* Norms */}
-                        <Box sx={{boxSizing:'border-box', width:'100%', flexGrow:1, overflow:'hidden'}}>
-                            <Box sx={{width:'100%', height:'100%', pl:'1rem', pr:'.5rem', boxSizing:'border-box', pb:'1rem'}}>
-                                <NormsList teamId={teamId} normsList={normsList} />
-                            </Box>
+                        <Box sx={{width:'33%', boxSizing:'border-box', px:'.5rem'}}>
+                            <NormsList teamId={teamId} normsList={normsList} />
                         </Box>
-                    </Box>
-                    {/* Agenda list */}
-                    <Box sx={{flexGrow:1, height:'100%', overflow:'hidden'}}>
-                        <Box sx={{width:'100%', height:'100%', pt:'.5rem', pr:'1rem', pl:'.5rem', boxSizing:'border-box', pb:'1rem'}}>
-                            {/* <AgendaList teamId={teamId} agendas={agendas} /> */}
+                        <Box sx={{width:'33%', boxSizing:'border-box', px:'.5rem'}}>
+                            <CyclesTasks cycles={cycles} />
                         </Box>
                     </Box>
                 </Box>
