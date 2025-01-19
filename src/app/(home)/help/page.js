040f167cbcc908d3462010bd42b47f1ccafce52d
Derek Huang <derekhuang7@gmail.com>
2025-01-19 13:10:01 -0800

    minor edits

--- src/app/(home)/help/page.js
+++ src/app/(home)/help/page.js
@@ -1,8 +1,6 @@
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
 import Fade from '@mui/material/Fade';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
@@ -10,10 +8,9 @@ import IconButton from '@mui/material/IconButton';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 
 
-import NextLink from 'next/link';
 import Image from "next/image";
-import ContactUs from "../contactUs";
 import VideoCard from "./videoCard";
+import Footer from "../footer";
 
 
 export default function Page() {
@@ -161,51 +158,7 @@ export default function Page() {
                     <VideoCard key={i} video={video} />
                 ))}
             </Box>
-            {/* Ending login */}
-            <Box sx={{width:'100%', height:'50%', backgroundColor: 'grey.300'}}>
-                <Box sx={{width:'100%', height:'100%', display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
-                    <Typography variant="h4" color="primary" align="center" sx={{fontWeight:'bold', display:'flex'}}>
-                        Start making&nbsp;
-                        <Typography variant="inherit" color="secondary" sx={{fontWeight:'inherit'}} component={'span'}>change</Typography> 
-                        &nbsp;happen.
-                    </Typography>
-                    <Button 
-                        variant="contained" disableElevation 
-                        sx={{borderRadius:3, textTransform:'none', mt:'2rem', py:'.75rem', px:'2rem'}}
-                        component={NextLink}
-                        href='/login'
-                        >
-                        <Typography variant="h6">
-                            Get started
-                        </Typography>
-                    </Button>
-                </Box>
-            </Box>
-            {/* footer */}
-            <Box sx={{width:'100%', display:'flex', boxSizing:'border-box', py:'2rem', backgroundColor:'common.white'}}>
-                <Box sx={{width:'30%', display:'flex', justifyContent:'center', alignItems:'flex-end', flexDirection:'column'}}>
-                    {/* logo and social */}
-                    <Typography variant="h3" color="primary" sx={{fontWeight:'bold'}}>LearnEd</Typography>
-                    {/* social */}
-                    <Box sx={{display:'flex', alignItems:'center'}}>
-                        {/* email */}
-                        <Box sx={{mr:'.5rem'}}>
-                            <NextLink target="_blank" href='mailto:derekhuang7@gmail.com'>
-                                <EmailRoundedIcon sx={{color:'common.black'}} />
-                            </NextLink>
-                        </Box>
-                        {/* linkedin */}
-                        <Box>
-                            <NextLink target="_blank" href='https://www.linkedin.com/in/derekjhuang/' >
-                                <Image src={'/linkedin-icon.png'} width={linkedInIconWidth} height={linkedInIconHeight} alt='linkedin' />
-                            </NextLink>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box sx={{width:'70%', display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
-                    <ContactUs />
-                </Box>
-            </Box>
+            <Footer />
         </Box>
     );
 }
--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -7,6 +7,7 @@ import NextLink from 'next/link';
 import DemoCarousel from "./demoCarousel";
 import Image from "next/image";
 import ContactUs from "./contactUs";
+import Footer from "./footer";
 
 
 export default function Page() {
@@ -44,51 +45,7 @@ export default function Page() {
                     <DemoCarousel />
                 </Box>
             </Box>
-            {/* Ending login */}
-            <Box sx={{width:'100%', height:'50%', backgroundColor: 'grey.300'}}>
-                <Box sx={{width:'100%', height:'100%', display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
-                    <Typography variant="h4" color="primary" align="center" sx={{fontWeight:'bold', display:'flex'}}>
-                        Start making&nbsp;
-                        <Typography variant="inherit" color="secondary" sx={{fontWeight:'inherit'}} component={'span'}>change</Typography> 
-                        &nbsp;happen.
-                    </Typography>
-                    <Button 
-                        variant="contained" disableElevation 
-                        sx={{borderRadius:3, textTransform:'none', mt:'2rem', py:'.75rem', px:'2rem'}}
-                        component={NextLink}
-                        href='/login'
-                        >
-                        <Typography variant="h6">
-                            Get started
-                        </Typography>
-                    </Button>
-                </Box>
-            </Box>
-            {/* footer */}
-            <Box sx={{width:'100%', display:'flex', boxSizing:'border-box', py:'2rem'}}>
-                <Box sx={{width:'30%', display:'flex', justifyContent:'center', alignItems:'flex-end', flexDirection:'column'}}>
-                    {/* logo and social */}
-                    <Typography variant="h3" color="primary" sx={{fontWeight:'bold'}}>LearnEd</Typography>
-                    {/* social */}
-                    <Box sx={{display:'flex', alignItems:'center'}}>
-                        {/* email */}
-                        <Box sx={{mr:'.5rem'}}>
-                            <NextLink target="_blank" href='mailto:derekhuang7@gmail.com'>
-                                <EmailRoundedIcon sx={{color:'common.black'}} />
-                            </NextLink>
-                        </Box>
-                        {/* linkedin */}
-                        <Box>
-                            <NextLink target="_blank" href='https://www.linkedin.com/in/derekjhuang/' >
-                                <Image src={'/linkedin-icon.png'} width={linkedInIconWidth} height={linkedInIconHeight} alt='linkedin' />
-                            </NextLink>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box sx={{width:'70%', display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
-                    <ContactUs />
-                </Box>
-            </Box>
+            <Footer />
         </Box>
     );
 }
