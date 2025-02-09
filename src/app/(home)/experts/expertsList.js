9ce36b56f9bfb9cb7ce2a4ceab612130cd52429b
Derek Huang <derekhuang7@gmail.com>
2025-02-09 11:25:00 -0800

    add ryoji

--- src/app/(home)/experts/expertsList.js
+++ src/app/(home)/experts/expertsList.js
@@ -13,5 +13,15 @@ export const expertsList = [
         email:'derekhuang7@gmail.com',
         website:'https://www.learnedplc.com'
     },
-
+    {
+        name: 'Ryoji Bunden',
+        role: 'Founder',
+        company: 'AchieveMetrics',
+        description: `18 years in education spanning from instruction to instructional coaching and instructional leadership. I've been fortunate to teach in the US and abroad in content areas of mathematics, AVID, andTheory of Knowledge. AP, IB, CCSS, CCR, PLC, Continuous Improvement, PDSA, and leading teams through standards implementation, understanding Smarter Balanced Item Specifications, Achievement Level Descriptors, and how to effectively measure student understanding. Student achievement is at the core of everything I do as an Educator and Educational Leader. We must be working to increase student achievement by leveraging Improvement Science and increasing educator capacity.`,
+        thumbnail:'/expert-profile-pic/ryojibunden.png',
+        aspectRatio: 1,
+        linkedIn: 'https://www.linkedin.com/in/ryoji-bunden-7052b9309/',
+        email:'rbunden@achievemetrics.com',
+        website:'https://www.achievemetrics.com/'
+    },
 ];
