2cb07ae74701453ba22a3b16b2e68fcf6d4dd540
Derek Huang <derekhuang7@gmail.com>
2025-02-22 13:04:41 -0800

    sped up tasks

--- src/app/u/tasks/page.js
+++ src/app/u/tasks/page.js
@@ -9,38 +9,49 @@ import PageClient from "./pageClient";
 
 export default async function Tasks({teamId, users}) {
     const supabase = await createClient();
-    let data;
-    let {data: teams} = await supabase
-        .from('teams')
-        .select(`
-            *,
-            team_memberships(*)
-        `);
+    // let data;
+    // let {data: teams} = await supabase
+    //     .from('teams')
+    //     .select(`
+    //         *,
+    //         team_memberships(*)
+    //     `);
+    // let teams = await getTeams(supabase);
 
     
-    if (teamId) {
-        data = (await supabase
-            .from('tasks')
-            .select()
-            .order('status')
-            .order('order_num')
-            .eq('team_id', teamId)).data;
-    } else {
-        data = (await supabase
-            .from('tasks')
-            .select(`
-                *,
-                teams(*)
-            `)
-            .order('status')
-            .order('order_num')).data;
-    }
+    // if (teamId) {
+    //     data = (await supabase
+    //         .from('tasks')
+    //         .select()
+    //         .order('status')
+    //         .order('order_num')
+    //         .eq('team_id', teamId)).data;
+    // } else {
+    //     data = (await supabase
+    //         .from('tasks')
+    //         .select(`
+    //             *,
+    //             teams(*)
+    //         `)
+    //         .order('status')
+    //         .order('order_num')).data;
+    // }
+    // const data = await getTasks(supabase, teamId);
 
-    const {data: [user], error: userError} = await supabase
-        .from('profiles')
-        .select();
+    // const {data: [user], error: userError} = await supabase
+    //     .from('profiles')
+    //     .select();
+    // const user = await getUser(supabase);
 
-    const teamMembers = users || (await supabase.rpc('get_users_on_same_team')).data;
+    // const teamMembers = users || (await supabase.rpc('get_users_on_same_team')).data;
+    // const teamMembers = await getTeamMembers(supabase, users);
+
+    const [teams, data, user, teamMembers] = await Promise.all([
+        getTeams(supabase),
+        getTasks(supabase, teamId),
+        getUser(supabase),
+        getTeamMembers(supabase, users)
+    ])
 
     return (
         <Box sx={{width:'100%', height:'100%', display:'flex', flexDirection:'column'}}>
@@ -61,4 +72,53 @@ export default async function Tasks({teamId, users}) {
             
         </Box>
     )
+}
+
+async function getTeams(supabase) {
+    let {data, error} = await supabase
+        .from('teams')
+        .select(`
+            *,
+            team_memberships(*)
+        `);
+
+    return data
+}
+
+async function getTasks(supabase, teamId) {
+    let data;
+
+    if (teamId) {
+        data = (await supabase
+            .from('tasks')
+            .select()
+            .order('status')
+            .order('order_num')
+            .eq('team_id', teamId)).data;
+    } else {
+        data = (await supabase
+            .from('tasks')
+            .select(`
+                *,
+                teams(*)
+            `)
+            .order('status')
+            .order('order_num')).data;
+    }
+
+    return data;
+}
+
+async function getUser(supabase) {
+    const {data: [user], error: userError} = await supabase
+        .from('profiles')
+        .select();
+    
+    return user;
+}
+
+async function getTeamMembers(supabase, users) {
+    const teamMembers = users || (await supabase.rpc('get_users_on_same_team')).data;
+
+    return teamMembers;
 }
